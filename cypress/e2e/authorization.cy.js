33aee1178feb79735fb25a44a5fe4e3669f55a51
schevcha <schevcha.work@gmail.com>
2023-05-16 17:59:44 +0300

    add sign-in test

--- cypress/e2e/authorization.cy.js
+++ cypress/e2e/authorization.cy.js
@@ -19,3 +19,17 @@ describe('Sign Up', () => {
         cy.get('.navbar').should('contain.text', username);
     })
 })
+
+describe('Sign In', () => {
+    it('should do login user', () => {
+        cy.visit('/');
+        cy.get('.navbar a[href$="/login"]').click();
+        cy.url().should('include', '/#/login');
+        cy.get('.auth-page h1').should('have.text', 'Sign in');
+        cy.get('.auth-page form').should('be.visible');
+        cy.get('.auth-page form input[ng-model$=email]').type('ff1f@gmail.com');
+        cy.get('.auth-page form input[ng-model$=password]').type('fff');
+        cy.get('.auth-page form button[type=submit]').click();
+        cy.get('.navbar').should('contain.text', 'ff1f');
+    })
+})
